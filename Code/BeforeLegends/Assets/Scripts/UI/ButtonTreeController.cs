ff37dbf3fba2f8e2429e76a5ae8132abfb540951
Viktor Pekar <vikpek@gmail.com>
2015-03-09 11:10:28 +0100

    first popup menu concept

---
+++ Code/BeforeLegends/Assets/Scripts/UI/ButtonTreeController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ButtonTreeController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+	
+	List<Button> secondOrderButtons = new List<Button>();
+	
+	// Use this for initialization
+	void Start () {		
+		foreach(Button child in GetComponentsInChildren<Button>())
+		{
+			if(child.tag == Tags.BUTTON_SECOND_ORDER)
+			{
+				secondOrderButtons.Add(child);
+				child.gameObject.SetActive( false );
+			}
+		}
+	}
+	
+	public void OnPointerDown(PointerEventData data)
+	{
+		setSecondOrderButtonsTo (true);
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+
+	{
+		setSecondOrderButtonsTo (false);
+	}
+
+	void setSecondOrderButtonsTo (bool state)
+	{
+		foreach (Button b in secondOrderButtons) {
+			b.gameObject.SetActive (state);
+		}
+	}
+}
