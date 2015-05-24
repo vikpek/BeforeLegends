53304c92130b867658b4b81e0acc8dfc45664a5b
Viktor Pekar <vikpek@gmail.com>
2015-05-24 11:41:22 +0200

    basic structure for simple battle one on one system

---
+++ Code/BeforeLegends/Assets/Scripts/BattleSystem/BattleActions.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var playerUnitData : UnitData;
+var enemyUnitData : UnitData;
+
+function Start () {
+	playerUnitData = GameObject.FindGameObjectWithTag("Player").GetComponent(BattleParameters).battleParameters;
+	enemyUnitData = GameObject.FindGameObjectWithTag("Enemy").GetComponent(BattleParameters).battleParameters;
+}
+
+function Update () {
+
+}
+
+function AttackDefault(){
+	Debug.Log("attack default" + playerUnitData.hitPoints);
+	
+//	player.GetComponent(BattleParameters);
+	
+}
+
+function Attack2(){
+	Debug.Log("attack 2 default");
+}
+
+function Defend(){
+	Debug.Log("defend default");
+}
