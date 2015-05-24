887cab239e3f84703d2fde0dd646bda26a3fb822
Viktor Pekar <vikpek@gmail.com>
2015-05-24 17:41:21 +0200

    api update backup

---
+++ Code/BeforeLegends/Assets/FaceToCamera.js
@@ -0,0 +1,13 @@
+﻿//#pragma strict
+//
+//var target : GameObject;
+////var camera : Camera;
+//
+//function Start() {
+////	camera = GetComponent(Camera);
+//}
+//
+//function Update() {
+//	var screenPos = camera.WorldToScreenPoint(target.transform.position);
+//	Debug.Log("target is " + screenPos.x + " pixels from the left");
+//}
