0d420386230fc2a63ab5c204fc6a6ed3c9f34604
Viktor Pekar <vikpek@gmail.com>
2015-05-24 15:38:33 +0200

    turnbased core system (detached from messenger system)

--- Code/BeforeLegends/Assets/Scripts/BattleSystem/BattleRoundController.js
+++ Code/BeforeLegends/Assets/Scripts/BattleSystem/BattleRoundController.js
@@ -1,18 +1,59 @@
-﻿#pragma strict
+﻿var action : String; var player : String; var battle = true; var displayGui = false;
 
-private var playerTurn = 0;
-private var reactionController;
-private var battleActions;
+function Start () { 
+	while (battle) { 
+		yield PlayerChoice(); 
+		yield BattlePhase(); 
+		yield TurnEnd();
 
-//function Start () {
-//	reactionController = GetComponent(BattleReactionsController);
-//	battleAction = GetComponent(BattleActions);
-//}
-//
-//function Update () {
-//	if(playerTurn==1){
-//		battleActions.enableActions();					
-//	}else if(playerTurn == 2){
-//		reactionController.performReaction();
-//	}
-//}
+     	yield EnemyChoice();
+     	yield BattlePhase();
+     	yield TurnEnd();
+ 	}
+}
+
+function PlayerChoice() { 
+	print("human makes a decision"); 
+	player = "human"; 
+	displayGui = true;
+
+ while (displayGui)
+     yield;        
+}
+
+function BattlePhase() { 
+	print("battle rages on"); 
+	yield new WaitForSeconds(1); 
+	print(player + " " + action); 
+	yield new WaitForSeconds(1); 
+}
+
+function EnemyChoice() { 
+	print("enemy makes a decision"); 
+	yield new WaitForSeconds(1); 
+	player = "enemy"; 
+	action = "attacks"; 
+}
+
+function TurnEnd() { 
+	print("turn ends"); 
+	yield new WaitForSeconds(1); 
+}
+
+function OnGUI() { 
+	if (!displayGui) return;
+
+ 	GUI.Box (Rect (10,10,100,90), "Battle Menu");
+ 
+	 if (GUI.Button (Rect (20,40,80,20), "Attack"))
+	 {
+	     action = "attacks";
+	     displayGui = false;
+	 } 
+	 
+	 if (GUI.Button (Rect (20,70,80,20), "Defend"))
+	 {
+	     action = "defends";
+	     displayGui = false;
+	 }
+}
