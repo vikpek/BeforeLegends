6f7866c5054ac22e57e2064c54aa6ef5eda6b949
TomaKomitski <toma_komitski@hotmail.com>
2015-05-28 15:14:37 +0200

    ga fade

---
+++ Code/BeforeLegends/Assets/Scripts/GAFade.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var time : float = 0;
+var maxTime : float;
+var sprite : SpriteRenderer;
+var fadeOut : boolean = false;
+
+function Update(){
+	sprite.color.a = Mathf.Lerp((fadeOut ? 1 : 0), (fadeOut ? 0 : 1) , time / maxTime);
+	if(time >= maxTime){
+		if(!fadeOut){
+			time = 0;
+			fadeOut = true;
+		}else{
+			Application.LoadLevel("MainMenu");
+		}
+	}
+	time += Time.deltaTime;
+}
