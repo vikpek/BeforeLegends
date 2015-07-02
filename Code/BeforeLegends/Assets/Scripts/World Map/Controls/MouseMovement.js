542ff35efa06aa105d246b69a3e7fc51ba14ef9d
lugeoul <lugeoul@gmail.com>
2015-07-02 14:34:53 +0200

    Have to change the branch. Don't want to use stash now

--- Code/BeforeLegends/Assets/Scripts/World Map/Controls/MouseMovement.js
+++ Code/BeforeLegends/Assets/Scripts/World Map/Controls/MouseMovement.js
@@ -4,6 +4,7 @@ var dampen : float;
 
 @HideInInspector
 var nextPos : Vector3;
+@HideInInspector
 var mouseScrollDeltaY : float;
 var maxY : float;
 var minY : float;
@@ -18,8 +19,6 @@ function Update () {
     mouseScrollDeltaY = 0;
     mouseScrollDeltaY = -Input.mouseScrollDelta.y * 1.5;
 
-    print(mouseScrollDeltaY);
-
     if(nextPos.y + mouseScrollDeltaY < minY && mouseScrollDeltaY < 0)
         nextPos.y = minY;
     else if(nextPos.y + mouseScrollDeltaY > maxY && mouseScrollDeltaY > 0)
@@ -30,7 +29,10 @@ function Update () {
 	var delta : Vector2 = Vector2.zero;
 	
 	if((Input.mousePosition.x > Screen.width - 25  || Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)) && nextPos.x < WorldMapGenerator.instance.size.x * xMultiplier){
-		delta.x += transform.right.x;
+		
+	    print("WorldMapGenerator.instance.size.x: " + WorldMapGenerator.instance.size.x);
+	    
+	    delta.x += transform.right.x;
 		delta.y += transform.right.z;
 		//nextPos.x += nextPos.y * Time.deltaTime;
 	}else if((Input.mousePosition.x < 25 || Input.GetKey(KeyCode.A)  || Input.GetKey(KeyCode.LeftArrow)) && nextPos.x > 0){
@@ -38,10 +40,10 @@ function Update () {
 		delta.y -= transform.right.z;
 	}
 	
-	if((Input.mousePosition.y > Screen.height - 25 || Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)) && nextPos.z > WorldMapGenerator.instance.size.y * zMultiplier){
+	if((Input.mousePosition.y > Screen.height - 25 || Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)) && nextPos.z < WorldMapGenerator.instance.size.y * zMultiplier){
 		delta.x += transform.up.x;
 		delta.y += transform.up.z;
-	}else if((Input.mousePosition.y < 25 || Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)) && nextPos.z < 0){
+	}else if((Input.mousePosition.y < 25 || Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)) && nextPos.z > 0){
 		delta.x -= transform.up.x;
 		delta.y -= transform.up.z;
 	}
