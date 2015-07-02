53568606637c0fab9092a5c53c57970c4f29fa0e
lugeoul <lugeoul@gmail.com>
2015-07-02 14:54:37 +0200

    Mouse movement fix....... again :D

--- Code/BeforeLegends/Assets/Scripts/World Map/Controls/MouseMovement.js
+++ Code/BeforeLegends/Assets/Scripts/World Map/Controls/MouseMovement.js
@@ -6,10 +6,11 @@ var dampen : float;
 var nextPos : Vector3;
 @HideInInspector
 var mouseScrollDeltaY : float;
-var maxY : float;
 var minY : float;
-var xMultiplier : float;
-var zMultiplier : float;
+var maxY : float;
+var xOffsetMultiplier : float;
+var zOffsetMultiplierMin : float;
+var zOffsetMultiplierMax : float;
 
 function Awake(){
 	nextPos = transform.position;
@@ -19,6 +20,7 @@ function Update () {
     mouseScrollDeltaY = 0;
     mouseScrollDeltaY = -Input.mouseScrollDelta.y * 1.5;
 
+
     if(nextPos.y + mouseScrollDeltaY < minY && mouseScrollDeltaY < 0)
         nextPos.y = minY;
     else if(nextPos.y + mouseScrollDeltaY > maxY && mouseScrollDeltaY > 0)
@@ -27,23 +29,20 @@ function Update () {
         nextPos.y += mouseScrollDeltaY;
 
 	var delta : Vector2 = Vector2.zero;
-	
-	if((Input.mousePosition.x > Screen.width - 25  || Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)) && nextPos.x < WorldMapGenerator.instance.size.x * xMultiplier){
-		
-	    print("WorldMapGenerator.instance.size.x: " + WorldMapGenerator.instance.size.x);
-	    
-	    delta.x += transform.right.x;
+
+	if((Input.mousePosition.x > Screen.width - 25  || Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)) && nextPos.x < WorldMapGenerator.instance.size.x * xOffsetMultiplier){
+		delta.x += transform.right.x;
 		delta.y += transform.right.z;
 		//nextPos.x += nextPos.y * Time.deltaTime;
 	}else if((Input.mousePosition.x < 25 || Input.GetKey(KeyCode.A)  || Input.GetKey(KeyCode.LeftArrow)) && nextPos.x > 0){
 		delta.x -= transform.right.x;
 		delta.y -= transform.right.z;
 	}
-	
-	if((Input.mousePosition.y > Screen.height - 25 || Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)) && nextPos.z < WorldMapGenerator.instance.size.y * zMultiplier){
+
+	if((Input.mousePosition.y > Screen.height - 25 || Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)) && nextPos.z < WorldMapGenerator.instance.size.y - ((transform.position.y - maxY) * zOffsetMultiplierMax)){
 		delta.x += transform.up.x;
 		delta.y += transform.up.z;
-	}else if((Input.mousePosition.y < 25 || Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)) && nextPos.z > 0){
+	}else if((Input.mousePosition.y < 25 || Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)) && nextPos.z > -transform.position.y * zOffsetMultiplierMin){
 		delta.x -= transform.up.x;
 		delta.y -= transform.up.z;
 	}
