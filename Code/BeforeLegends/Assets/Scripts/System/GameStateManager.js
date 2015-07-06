084cfe8b727d99cb7d69779577ba4c0955be7d30
lugeoul <lugeoul@gmail.com>
2015-07-06 15:09:59 +0200

    Battlemap GUI

--- Code/BeforeLegends/Assets/Scripts/System/GameStateManager.js
+++ Code/BeforeLegends/Assets/Scripts/System/GameStateManager.js
@@ -4,6 +4,8 @@ static var instance : GameStateManager;
 
 var world : GameObject;
 var battle : GameObject;
+var battleMapGui : GameObject;
+var worldMapGui : GameObject;
 
 var state : int = 0;
 var lastBattleResult : boolean = true;
@@ -17,12 +19,16 @@ function startBattle(player : GameObject, enemy : GameObject){
 	state = 1;
 	battle.SetActive(true);
 	world.SetActive(false);
+	battleMapGui.SetActive(true);
+	worldMapGui.SetActive(false);
 }
 
 function endBattle(result : boolean, exp : int){
 	state = 0;
 	world.SetActive(true);
 	battle.SetActive(false);
+	battleMapGui.SetActive(false);
+	worldMapGui.SetActive(true);
 	RessourceMaster.instance.valueScore += exp;
 	GameObject.Find("Olaf").GetComponent.<BattleParameters>().exp += exp;
 	GameObject.Find("Olaf").GetComponent.<BattleParameters>().LevelUp();
--- Code/BeforeLegends/Assets/Scripts/UI/UIFunctions.cs
+++ Code/BeforeLegends/Assets/Scripts/UI/UIFunctions.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIFunctions : MonoBehaviour {
 
-    int slideState = 0; // 0 ==  nothing, 1 == down, 2 == up
-    float slideYTime = 0;
+    int slideState = 0; // 0 ==  nothing, 1 == In, 2 == Out
+    int fillState = 0; // 0 ==  nothing, 1 == Fill, 2 == Unfill
+    float slideTime = 0;
+    float fillTime = 0;
 
     public Vector3 slideDistance;
-    public AnimationCurve slideY;
+    public AnimationCurve slide;
+    public AnimationCurve fill;
     
     RectTransform rTransform;
+    Image image;
     Vector3 startPosition;
+    public bool slideX;
+    public bool fillAndUnfill = false;
 
 	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         rTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
         startPosition = rTransform.position;
 	}
 
@@ -22,31 +31,92 @@ public class UIFunctions : MonoBehaviour {
 	void Update () {
         if(Input.GetKeyDown(KeyCode.H))
         {
-            print("slideY.Evaluate(slideYTime) * slideDistance.y: " + slideY.Evaluate(slideYTime) * slideDistance.y);
+            print("slideY.Evaluate(slideYTime) * slideDistance.y: " + slide.Evaluate(slideTime) * slideDistance.y);
         }
-	    switch(slideState)
+        if (slideX)
         {
-            case 1:
-                slideYTime += Time.deltaTime;
-                rTransform.position = new Vector3(rTransform.position.x, startPosition.y - (slideY.Evaluate(slideYTime) * slideDistance.y), rTransform.position.z);
-                if (slideYTime >= slideY.keys[slideY.keys.Length - 1].time)
-                    slideState = 0;
-                break;
-            case 2:
-                slideYTime -= Time.deltaTime;
-                rTransform.position = new Vector3(rTransform.position.x, startPosition.y - (slideY.Evaluate(slideYTime) * slideDistance.y), rTransform.position.z); 
-                if (slideYTime <= 0)
-                    slideState = 0;
-                break;
+	        switch(slideState)
+            {
+                case 1:
+                    slideTime += Time.deltaTime;
+                    rTransform.position = new Vector3(startPosition.x - (slide.Evaluate(slideTime) * slideDistance.x), rTransform.position.y, rTransform.position.z);
+                    if (slideTime >= slide.keys[slide.keys.Length - 1].time)
+                    {
+                        slideState = 0;
+                        slideTime = slide.keys[slide.keys.Length - 1].time;
+                    }
+                    break;
+                case 2:
+                    slideTime -= Time.deltaTime;
+                    rTransform.position = new Vector3(startPosition.x - (slide.Evaluate(slideTime) * slideDistance.x), rTransform.position.y, rTransform.position.z); 
+                    if (slideTime <= 0)
+                    {
+                        slideState = 0;
+                        slideTime = 0;
+                    }
+                    break;
+            }
+        } 
+        else
+        {
+            switch (slideState)
+            {
+                case 1:
+                    slideTime += Time.deltaTime;
+                    rTransform.position = new Vector3(rTransform.position.x, startPosition.y - (slide.Evaluate(slideTime) * slideDistance.y), rTransform.position.z);
+                    if (slideTime >= slide.keys[slide.keys.Length - 1].time)
+                    {
+                        slideState = 0;
+                        slideTime = slide.keys[slide.keys.Length - 1].time;
+                    }
+                    break;
+                case 2:
+                    slideTime -= Time.deltaTime;
+                    rTransform.position = new Vector3(rTransform.position.x, startPosition.y - (slide.Evaluate(slideTime) * slideDistance.y), rTransform.position.z);
+                    if (slideTime <= 0)
+                    {
+                        slideState = 0;
+                        slideTime = 0;
+                    }
+                    break;
+            }
+        }
+        if (fillAndUnfill)
+        {
+            switch (fillState)
+            {
+                case 1:
+                    fillTime += Time.deltaTime;
+                    image.fillAmount = fill.Evaluate(fillTime);
+                    if (fillTime >= fill.keys[fill.keys.Length - 1].time)
+                    {
+                        fillState = 0;
+                        fillTime = fill.keys[fill.keys.Length - 1].time;
+                    }
+                    break;
+                case 2:
+                    fillTime -= Time.deltaTime;
+                    image.fillAmount = fill.Evaluate(fillTime);
+                    if (fillTime <= 0)
+                    {
+                        fillState = 0;
+                        fillTime = 0;
+                    }
+                    break;
+        }
         }
 	}
 
-    public void SlideDown()
+    public void SlideIn()
     {
         slideState = 1;
+        fillState = 1;
     }
-    public void SlideUp()
+    public void SlideOut()
     {
         slideState = 2;
+        fillState = 2;
     }
+
+    
 }
