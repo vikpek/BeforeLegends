300afc8a790c8f0dd3f7c26c523f4f9c97b9a1c9
unknown <Benjamin.koss@2014-GA-25.games-academy.de>
2015-07-08 21:20:20 +0200

    Bugfixes YAY ^_^

---
+++ Code/BeforeLegends/Assets/Scripts/Utilities/FaceCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+    public Camera cam; 
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(cam.transform);
+	}
+}
